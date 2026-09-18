f6c69185a08051b621edcfaab35ef129c685e2a0
talha <talhavural316@gmail.com>
2026-09-18 17:41:04 +0300

    .

---
+++ Frontends/CarBook.Dto/BrandDtos/ResultBrandDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarBook.Dto.BrandDtos
+{
+    public class ResultBrandDto
+    {
+            public int BrandId { get; set; }
+            public string Name { get; set; }
+    }
+}
--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using CarBook.Dto.CarDtos;
+using CarBook.Dto.BrandDtos;
 
 namespace CarBook.WebUI.Controllers
 {
@@ -25,9 +27,22 @@ namespace CarBook.WebUI.Controllers
             }
             return View();
         }
+
         [HttpGet]
-        public IActionResult CreateCar()
+        public async Task<IActionResult> CreateCar()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7014/api/Brands");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.BrandId.ToString()
+                                                }).ToList();
+            ViewBag.BrandValues = brandValues;
             return View();
         }
     }
