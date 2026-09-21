6664fe50b58bea038633b3e4e3828a80901c7a01
talha <talhavural316@gmail.com>
2026-09-21 17:14:32 +0300

    .

---
+++ Frontends/CarBook.Dto/AboutDtos/CreateAboutDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarBook.Dto.AboutDtos
+{
+    public class CreateAboutDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Frontends/CarBook.Dto/AboutDtos/UpdateAboutDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarBook.Dto.AboutDtos
+{
+    public class UpdateAboutDto
+    {
+        public int AboutId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
